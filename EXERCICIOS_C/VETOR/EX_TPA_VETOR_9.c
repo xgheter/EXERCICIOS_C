4b56641c80eb73f4e102cb5c0addfe7d3513658d
xgheter <131470350+xgheter@users.noreply.github.com>
2023-08-16 02:15:20 +0000

    dei enter

--- EXERCICIOS_C/VETOR/EX_TPA_VETOR_9.c
+++ EXERCICIOS_C/VETOR/EX_TPA_VETOR_9.c
@@ -3,11 +3,11 @@ int main(){
     int v[5] = {45,-89,32,-12,33},digitado,i;
     for ( i = 0; i < 5; i++)
     {
-        for (v[i] = v[i]; v[i] < 89; v[i]--)
+        for (v[i] = v[i]; 100; v[i]--)
         {
-            if (v[0] == 0)
+            if (v[i] == -90)
             {
-                printf("%d",v[0]);
+                printf("%d",v[i]);
             }
             
             
