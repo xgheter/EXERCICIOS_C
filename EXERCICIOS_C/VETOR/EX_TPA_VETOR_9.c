7f339421081141492f451514156c0b8728e88247
xgheter <131470350+xgheter@users.noreply.github.com>
2023-08-21 02:34:48 +0000

    jesus cristo em ti confio

--- EXERCICIOS_C/VETOR/EX_TPA_VETOR_9.c
+++ EXERCICIOS_C/VETOR/EX_TPA_VETOR_9.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 int main(){
-    int v[5] = {45,-89,32,-12,33},var;
-    for (int i = 0; i < 4; i++)
+    int v[5] = {45,-89,32,-12,33},var,i,con;
+    for (i = 0; i < 4; i++)
     {
-        
+        for (int j=0; j < i-1 ;j++)
+        {
+
+            if (v[j] > v[j+1])
+            {
+                v[j] = v[j+1];
+            }
+        }
+              
     }
+    printf("%d",v);
     
 }
