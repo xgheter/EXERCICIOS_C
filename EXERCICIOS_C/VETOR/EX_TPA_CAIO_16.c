838a895fd29aed6b1787f22115b2610a2630c8fe
xgheter <131470350+xgheter@users.noreply.github.com>
2023-08-22 13:51:13 +0000

    socorro

---
+++ EXERCICIOS_C/VETOR/EX_TPA_CAIO_16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+    int v1[6] = {41,72,39,4,35},v2[6] = {0,0,0,0,0},s=0, r, n= 0;
+    for (int i = 0; i < 5; i++)
+    {
+        s = s + v1[i];
+    }
+   
+    r = s/5;
+    for (int i = 0; i < 5; i++)
+    {
+        if (v1[i] > r)
+        {
+            v2[i] = v1[i];
+        }
+        if (v2[i] > r )
+         {
+            n++;
+         }
+    }
+    printf("%d |",n);
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     if (v1[i] != 0)
+    //     {
+    //         printf("%d |",i);
+    //     }
+        
+    // }
+    
+    
+}
--- EXERCICIOS_C/VETOR/EX_TPA_VETOR_12.c
+++ EXERCICIOS_C/VETOR/EX_TPA_VETOR_12.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main(){
-    int v1[6] = {41,72,39,4,35},v2[6] = {0,0,0,0,0};
+    int v1[6] = {41,72,39,4,35},v2[6] = {0,0,0,0,0}, var, j=0;;
     for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 5; j++)
-        {
-            if (v1[i] < v1[j])
+        for (j = j; j < 5; j++) 
+        {  
+            if (v1[j] < v1[i])
             {
-                v1[j] = v1[i];  
-            }  
+                var = v1[i];
+                v1[i] = v1[j];
+            }
         } 
     }
     for (int i = 0; i < 5; i++)
     {
-        printf("%d",v1[i]);
+        printf("%d |",v1[i]);
     }
     
-}
+} //cade meu pix de 4000 ):<
