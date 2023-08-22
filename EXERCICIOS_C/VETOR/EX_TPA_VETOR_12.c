12a6c86f82d0ad6921033946375b5bc84070ddc3
xgheter <131470350+xgheter@users.noreply.github.com>
2023-08-22 02:52:29 +0000

    terminando o dia

--- EXERCICIOS_C/VETOR/EX_TPA_VETOR_12.c
+++ EXERCICIOS_C/VETOR/EX_TPA_VETOR_12.c
@@ -3,14 +3,17 @@ int main(){
     int v1[6] = {41,72,39,4,35},v2[6] = {0,0,0,0,0};
     for (int i = 0; i < 5; i++)
     {
-        for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
         {
-            if (v1[i] < v1[0])
+            if (v1[i] < v1[j])
             {
-                v1[0] = v1[i];      
-            }
-               
-        }   
+                v1[j] = v1[i];  
+            }  
+        } 
     }
-    printf("%d |",v1[0]);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d",v1[i]);
+    }
+    
 }
