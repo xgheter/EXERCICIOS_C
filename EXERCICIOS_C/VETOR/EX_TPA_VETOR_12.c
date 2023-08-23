6854e7c791b84607f6906e7b7dd050a2b0b94217
xgheter <131470350+xgheter@users.noreply.github.com>
2023-08-23 00:00:38 +0000

    meu deus meu senhor

--- EXERCICIOS_C/VETOR/EX_TPA_VETOR_12.c
+++ EXERCICIOS_C/VETOR/EX_TPA_VETOR_12.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 int main(){
-    int v1[6] = {41,72,39,4,35},v2[6] = {0,0,0,0,0}, var, j=0;;
+    int v1[6] = {41,72,39,4,35},v2[6] = {0,0,0,0,0}, var;
     for (int i = 0; i < 5; i++)
     {
-        for (j = j; j < 5; j++) 
+        for (int j = i; j < 5; j++) 
         {  
             if (v1[j] < v1[i])
             {
                 var = v1[i];
                 v1[i] = v1[j];
+                v1[i] = var;
             }
         } 
     }
     for (int i = 0; i < 5; i++)
     {
         printf("%d |",v1[i]);
-    }
-    
-} //cade meu pix de 4000 ):<
+    }    
+   
+}
